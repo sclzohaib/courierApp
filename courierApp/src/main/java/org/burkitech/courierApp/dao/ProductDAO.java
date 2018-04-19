439f41c0ab3fe4d3606b9248026a521f84853cfd
Wahaj <Wahaj@WIN-U1KD8HKL7AO>
2018-04-19 14:03:42 +0500

    wahaj

--- courierApp/src/main/java/org/burkitech/courierApp/dao/ProductDAO.java
+++ courierApp/src/main/java/org/burkitech/courierApp/dao/ProductDAO.java
@@ -1,8 +1,12 @@
 package org.burkitech.courierApp.dao;
 
+import java.util.List;
+
 import org.burkitech.courierApp.dto.Product;
 
 public interface ProductDAO {
 
 	boolean add(Product product);
+
+	List<Product> productList();
 }
